a5b438bb63da94ea318c37ac177b73793c9780dd
Stanislas Ormières <stanislas.ormieres@paraschool.com>
2014-01-03 14:54:09 +0100

    Formatting and a forgotten coma

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,33 +1,31 @@
 define(function(require, exports, module) {
     var _ = require('underscore'),
-    	$ = require('jquery')
-	logger = require("logger"),
-	qtip = require("qtip"),
-	cm = require("cookieManager");
+        $ = require('jquery'),
+        logger = require("logger"),
+        qtip = require("qtip"),
+        cm = require("cookieManager");
 
     logger.log('Main module loaded');
 
     var bootstrap = function ()
     {
-    	logger.log('bootstrap() invoked');
-	var lastLogin = cm.read('lastLogin');
-	if (lastLogin) {
-		$('footer .lastLogin').html(lastLogin);
-		cm.create('lastLogin', new Date());
-	} else {
-		cm.create('lastLogin', new Date());
-	}
-	$('h1').qtip({
-	    content: {
-	        title: 'qTip2 is a wonderful jQuery plugin!',
-		text: 'qTip2 has been loaded!'
-	    },
-	    position: { my: 'top center', at: 'bottom center' }
-	});
+        logger.log('bootstrap() invoked');
+        var lastLogin = cm.read('lastLogin');
+        if (lastLogin) {
+            $('footer .lastLogin').html(lastLogin);
+        }
+        cm.create('lastLogin', new Date());
+        $('h1').qtip({
+            content: {
+                title: 'qTip2 is a wonderful jQuery plugin!',
+                text: 'qTip2 has been loaded!'
+            },
+            position: { my: 'top center', at: 'bottom center' }
+        });
     };
 
     $(function() {
-    	logger.log('DOM is ready');
+        logger.log('DOM is ready');
         bootstrap();
     });
 });
--- public/js/app/main.js
+++ public/js/app/main.js
@@ -2,15 +2,15 @@
 requirejs.config({
     baseUrl: 'js/',
     paths: {
-    	"jquery":           '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min',
-        'underscore':       '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.0/underscore-min',
-        'text':             '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min',
-	'imagesloaded':     'vendor/imagesloaded.pkgd.min',
-	'qtip':             '//cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.min',
-        'logLevels':        'util/logLevels',
-        'logger':           'util/logger',
-        'cookieManager':    'util/cookieManager',
-        'logConf':          'app/log-conf'
+        "jquery":         '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min',
+        'underscore':     '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.0/underscore-min',
+        'text':           '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min',
+        'imagesloaded':   'vendor/imagesloaded.pkgd.min',
+        'qtip':           '//cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.min',
+        'logLevels':      'util/logLevels',
+        'logger':         'util/logger',
+        'cookieManager':  'util/cookieManager',
+        'logConf':        'app/log-conf'
     },
 });
 requirejs(["app/app"]);
