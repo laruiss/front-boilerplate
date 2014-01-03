1b22562aed9fd9e5120119048046f412e4d0c6c4
Stanislas Ormières <stanislas.ormieres@paraschool.com>
2014-01-03 14:46:55 +0100

    Launch the default browser with the index.html page from server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app);
+	open = require('open'),
+	server = require('http').createServer(app),
+	port = 8088;
 
-server.listen(8007);
+server.listen(port);
 
 app.use("/", express.static(__dirname + '/public'));
 
+open('http://localhost:' + port);
