bbaae3e92b9cfabb157b58c46ceeaa61f762d20f
Stanislas Ormières <stan@stormier.ninja>
2019-04-24 16:12:12 +0200

    Update flow types

--- flow-typed/npm/chai_v4.x.x.js
+++ flow-typed/npm/chai_v4.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 5cfeddfc9de4ef171e6b11554add9b1c
-// flow-typed version: 30784ea04e/chai_v4.x.x/flow_>=v0.25.0
+// flow-typed signature: 3d012055643ffc7325a13636ed48af8a
+// flow-typed version: f04d291d8b/chai_v4.x.x/flow_>=v0.25.0
 
 declare module "chai" {
   declare type ExpectChain<T> = {
@@ -21,6 +21,7 @@ declare module "chai" {
     deep: ExpectChain<T>,
     any: ExpectChain<T>,
     all: ExpectChain<T>,
+    own: ExpectChain<T>,
 
     a: ExpectChain<T> & ((type: string, message?: string) => ExpectChain<T>),
     an: ExpectChain<T> & ((type: string, message?: string) => ExpectChain<T>),
@@ -57,6 +58,8 @@ declare module "chai" {
     lengthOf: (value: number, message?: string) => ExpectChain<T>,
 
     match: (regex: RegExp, message?: string) => ExpectChain<T>,
+    matches: (regex: RegExp, message?: string) => ExpectChain<T>,
+
     string: (string: string, message?: string) => ExpectChain<T>,
 
     key: (key: string) => ExpectChain<T>,
@@ -127,17 +130,21 @@ declare module "chai" {
     eventually: ExpectChain<T>,
     resolvedWith: (value: mixed) => Promise<mixed> & ExpectChain<T>,
     resolved: () => Promise<mixed> & ExpectChain<T>,
-    rejectedWith: (value: mixed) => Promise<mixed> & ExpectChain<T>,
+    rejectedWith: (
+      value: mixed,
+      errMsgMatcher?: RegExp | string,
+      msg?: string
+    ) => Promise<mixed> & ExpectChain<T>,
     rejected: () => Promise<mixed> & ExpectChain<T>,
     notify: (callback: () => mixed) => ExpectChain<T>,
     fulfilled: () => Promise<mixed> & ExpectChain<T>,
 
     // chai-subset
-    containSubset: (obj: Object | Object[]) => ExpectChain<T>,
+    containSubset: (obj: {} | Array< {} >) => ExpectChain<T>,
 
     // chai-redux-mock-store
     dispatchedActions: (
-      actions: Array<Object | ((action: Object) => any)>
+      actions: Array<{} | ((action: {}) => any)>
     ) => ExpectChain<T>,
     dispatchedTypes: (actions: Array<string>) => ExpectChain<T>,
 
@@ -226,8 +233,8 @@ declare module "chai" {
     static typeOf(val: mixed, type: string, msg?: string): void;
     static notTypeOf(val: mixed, type: string, msg?: string): void;
 
-    static instanceOf(val: mixed, constructor: Function, msg?: string): void;
-    static notInstanceOf(val: mixed, constructor: Function, msg?: string): void;
+    static instanceOf(val: mixed, constructor: Class< * >, msg?: string): void;
+    static notInstanceOf(val: mixed, constructor: Class< * >, msg?: string): void;
 
     static include(exp: string, inc: mixed, msg?: string): void;
     static include<T>(exp: Array<T>, inc: T, msg?: string): void;
